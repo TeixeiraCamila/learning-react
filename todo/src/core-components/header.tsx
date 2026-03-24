63bec2882a5f52a6180a7ce97036044621d3af41
TeixeiraCamila <teixeira0101camila@gmail.com>
2026-03-24 15:32:56 -0300

    [add] regras de negócio [update] badges de resumo das tarefas

--- todo/src/core-components/header.tsx
+++ todo/src/core-components/header.tsx
@@ -3,8 +3,9 @@ import Container from "../components/container"
 import Logo from '../assets/images/logo-icon.svg?react'
 export default function Header() {
   return <>
-    <Container as="header" className="mt-3 md:mt-20">
-      <Logo className="h-9 md:h-12"/>
+    <Container as="header" className="mt-3 md:mt-20 flex items-center gap-3">
+      <Logo className="h-9 md:h-12"/> 
+      <h1 className="text-2xl font-bold">Todo App</h1>
     </Container>
   </>
 }
--- todo/src/core-components/tasks-summary.tsx
+++ todo/src/core-components/tasks-summary.tsx
@@ -5,14 +5,13 @@ import useTasks from "../hooks/use-all-tasks"
 export default function TasksSummary() {
   const { createdTasksCount, concluedTasksCount } = useTasks();
 
-
   return <>
     <div className="flex items-center gap-2">
       <Text variant="body-sm-bold" className="!text-gray-300">Tarefas criadas</Text>
       <Badge variant="secundary">{createdTasksCount}</Badge>
     </div>
     <div className="flex items-center gap-2">
-      <Text variant="body-sm-bold" className="!text-gray-300">Tarefas concluídas</Text>
+      <Text variant="body-sm-bold" className="text-gray-300!">Tarefas concluídas</Text>
       <Badge variant="primary">{concluedTasksCount} de {createdTasksCount}</Badge>
     </div>
   </>
